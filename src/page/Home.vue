ec981edf6e611167091b724f81d1e860d8d64b5a
Nekocup <d07405009@ems.npu.edu.tw>
2021-02-27 01:05:47 +0800

    init

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <img src="@/assets/home/banner.jpg" alt="" />
+  </div>
 </template>
 
 <script>
@@ -7,3 +9,13 @@ export default {
   name: 'Home'
 }
 </script>
+
+<style lang="scss" scoped>
+img {
+  display: block;
+  width: 100%;
+  max-height: 450px;
+  object-fit: cover;
+  object-position: top bottom;
+}
+</style>
